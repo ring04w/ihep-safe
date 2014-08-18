4ec721d985387be65216a268892d40f6e989a934
wcc526 <949409306@qq.com>
2014-08-18 09:37:58 +0800

    update deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,111 +1,101 @@
-# config valid only for Capistrano 3.1
 lock '3.2.1'
 
 set :application, 'ihep-safe'
-set :repo_url, 'git@github.com:wcc526/ihep-safe.git'
+set :repo_url, "git@github.com:wcc526/#{fetch(:application)}.git"
 
-# Default branch is :master
-# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
+set :deploy_to, '/var/www/my_app'
 
-# Default deploy_to directory is /var/www/my_app
-set :deploy_to, '/home/apps/my_project'
-
-# Default value for :scm is :git
-set :scm, :git
-
-# Default value for :format is :pretty
-set :format, :pretty
-
-# Default value for :log_level is :debug
-set :log_level, :debug
-
-# Default value for :pty is false
-set :pty, true
-
-# Default value for :linked_files is []
-set :linked_files, %w{config/application.yml}
-
-# Default value for linked_dirs is []
+set :linked_files, %w{config/database.yml config/application.yml}
 set :linked_dirs, %w{results bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for keep_releases is 5
-# set :keep_releases, 5
 
 namespace :deploy do
 
   desc 'Restart application'
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
-      # Your restart mechanism here, for example:
       execute :touch, release_path.join('tmp/restart.txt')
     end
   end
 
   after :publishing, :restart
+  after :finishing, :cleanup
 
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
+  desc "Makes sure local git is in sync with remote."
+  task :check_revision do
+    unless `git rev-parse HEAD` == `git rev-parse origin/master`
+        puts "WARNING: HEAD is not the same as origin/master"
+        puts "Run `git push` to sync changes."
+      exit
     end
   end
+
+  %w[start stop restart].each do |command|
+  desc "#{command} Unicorn server."
+    task command do
+      on roles(:app) do
+        execute "/etc/init.d/unicorn_init #{command}"
+      end
+    end
+  end
+
+
+  before :deploy, "deploy:check_revision"
+  after :deploy, "deploy:restart"
+  after :rollback, "deploy:restart"
 
 end
 
 namespace :setup do
 
-  desc "SCP transfer figaro configuration to the shared folder"
-    task :upload_yml do
-      on roles(:app) do
-        upload! "config/application.yml", "#{shared_path}/config/application.yml", via: :scp
-      end
+  desc "Upload database.yml file."
+  task :upload_yml do
+    on roles(:app) do
+      upload! "config/database.yml", "#{shared_path}/config/database.yml",via: :scp
+      upload! "config/application.yml", "#{shared_path}/config/application.yml",via: :scp
+    end
   end
 
   desc "SCP transfer scan results to the shared folder"
-    task :upload_results do
-      on roles(:app) do
-        upload! "results", "#{shared_path}/", via: :scp, recursive: true
-      end
+  task :upload_dir do
+    on roles(:app) do
+      upload! "results", "#{shared_path}/", via: :scp, recursive: true
+    end
   end
 
   desc "Seed the database."
-      task :seed_db do
-        on roles(:app) do
-          within "#{current_path}" do
-          with rails_env: :production do
+  task :seed_db do
+    on roles(:app) do
+      within "#{current_path}" do
+        with rails_env: :production do
           execute :rake, "db:seed"
         end
       end
     end
   end
 
-  desc "ipdb sync"
-      task :ipdb_sync do
-        on roles(:app) do
-          within "#{current_path}" do
-          with rails_env: :production do
-          execute :rake, "ipdb:sync"
-        end
-      end
-    end
-  end
+   desc "ipdb sync"
+   task :ipdb_sync do
+     on roles(:app) do
+       within "#{current_path}" do
+         with rails_env: :production do
+           execute :rake, "ipdb:sync"
+         end
+       end
+     end
+   end
 
-  desc "ipdb scan"
-      task :ipdb_scan do
-        on roles(:app) do
-          within "#{current_path}" do
-          with rails_env: :production do
-          execute :rake, "ipdb:scan"
-        end
-      end
-    end
-  end
+   desc "ipdb scan"
+   task :ipdb_scan do
+     on roles(:app) do
+       within "#{current_path}" do
+         with rails_env: :production do
+           execute :rake, "ipdb:scan"
+         end
+       end
+     end
+   end
 
 end
 
 before "deploy:check:linked_files", "setup:upload_yml"
+after  "deploy:finishing","setup:seed_db"
